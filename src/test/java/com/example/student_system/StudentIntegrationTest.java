20b341f0e853b64ccd06266ab5c37f3544a6f0cd
FlaEroChek <vikm802@gmail.com>
2026-05-02 13:27:55 +0300

    Add integration tests

--- src/test/java/com/example/student_system/StudentIntegrationTest.java
+++ src/test/java/com/example/student_system/StudentIntegrationTest.java
@@ -35,4 +35,43 @@ class StudentIntegrationTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    void shouldReturnAllStudents() throws Exception {
+        mockMvc.perform(post("/students")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                            {
+                              "fullName":"Test User",
+                              "email":"test@test.com",
+                              "groupName":"IT-21",
+                              "averageGrade":4.5
+                            }
+                            """))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get("/students"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldReturnBadRequestOnDuplicateEmail() throws Exception {
+        String json = """
+            {
+              "fullName":"Ivan",
+              "email":"duplicate@test.com",
+              "groupName":"IT-21",
+              "averageGrade":4.5
+            }
+            """;
+
+        mockMvc.perform(post("/students")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/students")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isBadRequest());
+    }
 }
